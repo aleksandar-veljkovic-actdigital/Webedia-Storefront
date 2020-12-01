04ead3a295bc0aa26a753c325b5b9595a8a02830
Aleksandar <aleksandar.veljkovic@webedia-group.com>
2020-12-01 12:47:54 +0100

    es api.

--- plugins/api-es.js
+++ plugins/api-es.js
@@ -4,11 +4,7 @@ import {config} from '~/config'
 const apiBase = axios.create({
   baseURL: config.elasticsearch.api,
   baseURL: "http://localhost:3000/api/es",
-  headers: {'Content-Type': 'application/json'},
-  // withCredentials: true,
-  // httpsAgent: new https.Agent({
-  //   rejectUnauthorized: false,
-  // })
+  // headers: {'Content-Type': 'application/json;charset=UTF-8'},
 });
 
 const apiEs = {
@@ -18,15 +14,15 @@ const apiEs = {
     //   'Content-Type': 'application/json;charset=UTF-8',
     // }
     try {
-      var response = await apiBase.get(`/vue_storefront_catalog_${indexName}/_search`, config);
+      var request = await apiBase.get(`/vue_storefront_catalog_${indexName}/_search`, config);
     }
     catch (err) {
       console.error("ES Failed", {err})
-      var response = err.response
+      var request = err.response
     }
     return {
-      response,
-      payload: response?.data?.hits,
+      request,
+      response: request?.data,
     };
   },
   
