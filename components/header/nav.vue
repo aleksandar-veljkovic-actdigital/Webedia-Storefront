ebc1ab90cc1e18af495f0e29d08dc024d10778c1
Aleksandar <aleksandar.veljkovic@webedia-group.com>
2020-12-17 19:07:56 +0100

    nav...

--- components/header/nav.vue
+++ components/header/nav.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="c--header--nav">
     
+    <n-link :to="localePath('/')">home</n-link>
     <n-link :to="localePath('/shop')">shop</n-link>
     <n-link :to="localePath('/test')">test</n-link>
 
